321b172cb12f90d69e14efa90f46cea03a8029a4
sathish-13758 <sathish.sakthivel@zohocorp.com>
2024-12-20 14:10:25 +0530

    initial files added

---
+++ settings.gradle.kts
@@ -0,0 +1,19 @@
+enableFeaturePreview("TYPESAFE_PROJECT_ACCESSORS")
+pluginManagement {
+    repositories {
+        google()
+        gradlePluginPortal()
+        mavenCentral()
+    }
+}
+
+dependencyResolutionManagement {
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "Expense_tracker"
+include(":androidApp")
+include(":shared")
